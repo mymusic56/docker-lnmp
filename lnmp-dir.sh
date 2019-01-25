bc215720c96cd580a908e2e7b34f4a8db7abe848
mymusic56 <961272668@qq.com>
2019-01-26 00:31:49 +0800

    init

---
+++ lnmp-dir.sh
@@ -0,0 +1,12 @@
+mkdir -p /home/www/mysql/conf
+mkdir -p /home/www/mysql/mysql-files
+mkdir -p /home/www/mysql/var
+mkdir -p /home/www/mysql/data
+mkdir -p /home/www/nginx/conf/
+mkdir -p /home/www/nginx/conf/vhost
+mkdir -p /home/www/php/conf/
+
+cp my.cnf /home/www/mysql/conf
+cp php.ini /home/www/php/conf/
+cp php-fpm.conf /home/www/php/conf/
+cp mytest.com.conf /home/www/nginx/conf/vhost
